b9cfd356d5b4964c34ad5a5557f6f67219870d30
BG-coderno1 <goelbhavya06@gmail.com>
2024-08-26 17:24:19 +0530

    my first cpp project

---
+++ p-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+cout<<"Hi\n" <<endl;
+cout<<"I am Bhavya Goel\n" <<endl;
+cout<<"Branch: IT-3\n" <<endl;
+cout<<"Roll No:24/A08/044\n" <<endl;
+cout<<"pHone No:7982969225\n" <<endl;
+return 0;
+}
